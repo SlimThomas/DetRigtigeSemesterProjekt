6d890f95a0588b3e1289e95bf9e2a70e1d3230de
SlimThomas <147144450+SlimThomas@users.noreply.github.com>
2023-12-11 13:09:04 +0100

    Lavet dele af Json og lavet builder til program for Json

---
+++ Services/JsonFileHoldService.cs
@@ -0,0 +1,43 @@
+﻿using DetRigtigeSemesterProjekt.Models;
+using System.Text.Json;
+
+namespace DetRigtigeSemesterProjekt.Services
+{
+    public class JsonFileHoldService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "Hold.json"); }
+        }
+
+        public JsonFileHoldService(IWebHostEnvironment webHostEnvironment) 
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public void SaveJsonHold(List<Hold>holdliste)
+        {
+            using (FileStream jsonFileWriter = File.Create(JsonFileName))
+            {
+                Utf8JsonWriter jsonWriter = new Utf8JsonWriter(jsonFileWriter, new JsonWriterOptions()
+                {
+                    SkipValidation = false,
+                    Indented = true
+                });
+                JsonSerializer.Serialize<Hold[]>(jsonWriter, holdliste.ToArray()); 
+            }
+        }
+
+        public IEnumerable<Hold> GetJsonHold()
+        {
+            using (StreamReader jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Hold[]>(jsonFileReader.ReadToEnd()); 
+            }
+        }
+       
+
+    }
+}
