222e36046cb811be1a7d341722a3e5e7ee544ee1
LucasRing420 <lua001@edu.zealand.dk>
2023-12-11 12:24:41 +0100

    CRUD Location

---
+++ Pages/CRUDs/EditLocation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DetRigtigeSemesterProjekt.Pages.CRUDs
+{
+    public class EditLocationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
